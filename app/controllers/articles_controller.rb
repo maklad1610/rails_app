fa293ffad8f6358201a28c8f5259932088d96948
Fares Makled <faresmohammed1610@gmail.com>
2026-08-22 03:35:49 +0300

    Fix lint issues

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,6 @@ class ArticlesController < ApplicationController
     end
 
     def show
-    
-       @article = Article.find(params[:id]) 
+       @article = Article.find(params[:id])
     end
 end
