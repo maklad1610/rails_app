7e1a79bdb1c9fc2820b21395d097eca99bbbf4c4
Fares Makled <faresmohammed1610@gmail.com>
2026-08-23 00:29:38 +0300

    Delete Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,27 +1,27 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.all
+         @articles = Article.all
     end
 
     def show
-       @article = Article.find(params[:id])
+         @article = Article.find(params[:id])
     end
 
     def new
-     @article = Article.new
+         @article = Article.new
     end
 
     def edit
-        @article = Article.find(params[:id])
+         @article = Article.find(params[:id])
     end
 
     def create
         @article = Article.new(params.require(:article).permit(:title,:description))
         if @article.save
-            flash[:notice] = "Article was created successfully."
-            redirect_to article_path(@article)
+             flash[:notice] = "Article was created successfully."
+             redirect_to article_path(@article)
         else
-            render 'new'
+             render 'new'
         end    
     end
 
@@ -29,9 +29,15 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         if @article.update(params.require(:article).permit(:title,:description))
              flash[:notice] = "Article was edited successfully."
-            redirect_to article_path(@article)
+             redirect_to article_path(@article)
         else
-            render 'edit'
+             render 'edit'
         end   
+    end
+
+    def destroy
+         @article = Article.find(params[:id])
+         @article.destroy
+         redirect_to articles_path
     end
 end
