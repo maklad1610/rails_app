ce2987cf310a53a571dde9137e9a2c1b8743fa9e
Fares Makled <faresmohammed1610@gmail.com>
2026-09-03 00:41:03 +0300

    Add one-to-many associations between users and articles from backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, 
+    belongs_to :user
+    validates :title, presence: true,
                       length: { minimum: 5, maximum: 100 }
-    validates :description, presence: true, 
+    validates :description, presence: true,
                       length: { minimum: 10, maximum: 500 }
 end
