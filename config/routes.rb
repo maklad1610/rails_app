2fea89af15dc667e6673df1ad53a83f3db42e40b
Fares Makled <faresmohammed1610@gmail.com>
2026-09-14 20:09:04 +0300

    Add sessions create and destroy for logging users in and out

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
   resources :articles
   get "signup", to: "users#new"
-
   resources :users, except: [ :new ]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
